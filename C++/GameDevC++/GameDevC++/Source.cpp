115eff27455973b944059ab79dacc19519bb45b4
Rajyavardhan Karra <karrarajyavardhan@gmail.com>
2025-08-04 22:55:53 -0500

    C++ for GameDev and Comptia A+

---
+++ C++/GameDevC++/GameDevC++/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char myCharacter;
+	myCharacter = 'y';
+
+	int myInt;
+	myInt = 13;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+
+	myCharacter = 'n';
+	myInt = 12;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+
+	system("pause");
+}
