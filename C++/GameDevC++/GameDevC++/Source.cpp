be61f873766a9065d3a8a19f3ccaef05fe3b03c1
Rajyavardhan Karra <karrarajyavardhan@gmail.com>
2025-08-06 17:11:11 -0500

    Users Groups And Permissions

--- C++/GameDevC++/GameDevC++/Source.cpp
+++ C++/GameDevC++/GameDevC++/Source.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void welcome()
+{
+	cout << "Welcome to the game!" << endl;
+}
+
+void printNumber(int number)
+{
+	cout << "The number is: " << number << endl;
+} 
+
+int add(int a, int b)
+{
+	int result = a + b;
+	return result;
+}
+
 int main()
 {
+
+	welcome();
+
+	printNumber(5);
+
+	int c = add(3, 4);
+
+	printNumber(c); 
+
 	char myCharacter;
 	myCharacter = 'y';
 
